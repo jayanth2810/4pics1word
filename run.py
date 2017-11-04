656ab25fb2eb91fce9448b825484e80bd931865e
jayanth <Jayanth@codenations-MacBook-Pro.local>
2017-11-04 20:08:48 +0530

    Resetting connection details

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import os
 from app import app
 
 port = int(os.environ.get('PORT', 5000))  # for taking Heroku's PORT environment variable
-app.run(host='https://***.herokuapp.com:443', port=port, debug=False)
+app.run(host='0.0.0.0', port=port, debug=False)

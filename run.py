44283e451749ef8cd250a204fe18591dc5320cdf
jayanth <Jayanth@codenations-MacBook-Pro.local>
2017-11-04 20:01:24 +0530

    Changing run.py to a different host to check for HTTPS functionality - Part 2

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import os
 from app import app
 
 port = int(os.environ.get('PORT', 5000))  # for taking Heroku's PORT environment variable
-app.run(host='https://***.herokuapp.com:443/clue-generator-4pic1word.herokuapp.com', port=port, debug=False)
+app.run(host='https://***.herokuapp.com:443', port=port, debug=False)
